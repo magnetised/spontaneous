1ca8f991cdbc030acfd8d31336ff9f1b3ac1fa62
Garry Hill <garry@magnetised.net>
2015-09-22 15:33:25 +0100

    Don't try to fix the visibility issues in the migration

--- db/migrations/20150320130000_fix_hidden_origins.rb
+++ db/migrations/20150320130000_fix_hidden_origins.rb
@@ -8,24 +8,28 @@ Sequel.migration do
   up do
     # Don't run unless we're in a full Spontaneous instance i.e. not in a
     # testing environment
-    if defined?(Content)
-      aliases = Content.exclude(target_id: nil)
-      aliases.each do |a|
-        target = a.target
-        if target.hidden?
-          a.send :apply_set_visible, false, target.id
-          a.save
-        else
-          # The bug set the hidden origin to the id of the parent, not the target
-          # so we want to clear that unless the alias was actually hidden directly
-          # i.e. has a hidden_origin == nil
-          unless a.hidden_origin.nil?
-            a.send :apply_set_visible, true, nil
-            a.save
-          end
-        end
-      end
-    end
+    # These are all the pages that have been directly hidden
+    hidden = self[:content].where(hidden: true, hidden_origin: nil)
+    puts "Run #hide! on these ids when the migrations have completed:"
+    p    hidden.map { |row| row[:id] }
+    # if defined?(Content)
+    #   aliases = Content.exclude(target_id: nil)
+    #   aliases.each do |a|
+    #     target = a.target
+    #     if target.hidden?
+    #       a.send :apply_set_visible, false, target.id
+    #       a.save
+    #     else
+    #       # The bug set the hidden origin to the id of the parent, not the target
+    #       # so we want to clear that unless the alias was actually hidden directly
+    #       # i.e. has a hidden_origin == nil
+    #       unless a.hidden_origin.nil?
+    #         a.send :apply_set_visible, true, nil
+    #         a.save
+    #       end
+    #     end
+    #   end
+    # end
   end
 
   down do
